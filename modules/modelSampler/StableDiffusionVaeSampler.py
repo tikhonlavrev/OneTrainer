f73bd11049a648b4df765c1ee7859de6c86daf70
tikhonlavrev <101032353+tikhonlavrev@users.noreply.github.com>
2024-05-02 18:10:59 +0700

    StableDiffusionVaeSampler.py Accelerate Experimental

--- modules/modelSampler/StableDiffusionVaeSampler.py
+++ modules/modelSampler/StableDiffusionVaeSampler.py
@@ -5,6 +5,7 @@ from typing import Callable
 import torch
 from PIL import Image
 from torchvision.transforms import transforms
+from accelerate import Accelerator
 
 from modules.model.StableDiffusionModel import StableDiffusionModel
 from modules.modelSampler.BaseModelSampler import BaseModelSampler
@@ -16,15 +17,16 @@ from modules.util.config.SampleConfig import SampleConfig
 class StableDiffusionVaeSampler(BaseModelSampler):
     def __init__(
             self,
-            train_device: torch.device,
-            temp_device: torch.device,
             model: StableDiffusionModel,
             model_type: ModelType,
     ):
-        super(StableDiffusionVaeSampler, self).__init__(train_device, temp_device)
+        super(StableDiffusionVaeSampler, self).__init__()
 
         self.model = model
         self.model_type = model_type
+        self.accelerator = Accelerator()
+
+        print(f"The StableDiffusionVAESample is using: {self.accelerator.state.device} with {self.accelerator.num_processes} processes")
 
     def sample(
             self,
@@ -41,16 +43,14 @@ class StableDiffusionVaeSampler(BaseModelSampler):
         image = image.convert("RGB")
 
         t_in = transforms.ToTensor()
-        image_tensor = t_in(image).to(device=self.train_device, dtype=self.model.vae.dtype)
+        image_tensor = t_in(image).to(device=self.accelerator.device, dtype=self.model.vae.dtype)
         image_tensor = image_tensor * 2 - 1
 
-        self.model.vae_to(self.train_device)
+        self.model, self.optimizer = self.accelerator.prepare(self.model)
 
         with torch.no_grad():
             latent_image_tensor = self.model.vae.encode(image_tensor.unsqueeze(0)).latent_dist.mean
             image_tensor = self.model.vae.decode(latent_image_tensor).sample.squeeze()
-
-        self.model.vae_to(self.temp_device)
 
         image_tensor = (image_tensor + 1) * 0.5
         image_tensor = image_tensor.clamp(0, 1)
